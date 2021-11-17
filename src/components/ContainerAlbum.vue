7a46a6e1d5c7471e25fdf82c05f0f9dbe95bc92d
ddecataldo <davidedecataldo92@gmail.com>
2021-11-17 19:33:45 +0100

    Esercizio finito

--- src/components/ContainerAlbum.vue
+++ src/components/ContainerAlbum.vue
@@ -1,11 +1,20 @@
 <template>
     <main>
         <div class="container">
+
+            <div class="row">
+                <div class="col-md-12">
+                    
+                    <Select @inizioRicerca="filtroGenere"></Select>
+
+                </div>
+            </div>
+
             <div class="row row-cols-1 row-cols-md-5 g-4 py-5">
                 <div
-                class="col"
-                v-for="(album, i) in albumList"
-                :key="i"
+                    class="col"
+                    v-for="(album, i) in listaAlbumFiltrati"
+                    :key="i"
                 >
                 <CardAlbum
                     :src="album.poster"
@@ -23,16 +32,19 @@
 <script>
 import axios from "axios";
 import CardAlbum from "./CardAlbum.vue";
+import Select from "./Select.vue";
 
 export default {
     name: "ContainerAlbum",
     components: {
         CardAlbum,
+        Select,
     },
       data() {
         return {
             urlApi: "https://flynn.boolean.careers/exercises/api/array/music",
             albumList: [],
+            genereScelto: "",
         };
     },
     methods: {
@@ -45,11 +57,28 @@ export default {
             });
 
         },
+        filtroGenere(genereDigitato) {
+            this.genereScelto = genereDigitato;
+            /* console.log(genereDigitato); */
+        },
     },
     mounted(){
         this.fetchData();
+    },
+    computed: {
+        listaAlbumFiltrati() {
+
+            if (!this.genereScelto) {
+                return this.albumList;
+            }
+
+            return this.albumList.filter(elemento => {
+                return elemento.genre.toLowerCase().includes(this.genereScelto.toLowerCase())
+            });
+            
+            },
+        },
     }
-}
 
 </script>
 
---
+++ src/components/Select.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <div class="input-group mt-5">
+        
+        <select class="form-select" aria-label="Default select example"
+        v-model="genereUtente"
+        @keydown.enter="avviaRicerca">
+            <option value="">Tutti</option>
+            <option value="rock">Rock</option>
+            <option value="metal">Metal</option>
+            <option value="pop">Pop</option>
+            <option value="jazz">Jazz</option>
+        </select>
+
+        <button class="btn btn-outline-primary" type="button"
+        @click="avviaRicerca"
+        >Ricerca</button>
+
+    </div>
+
+    
+</template>
+
+<script>
+export default {
+    name: "BarraDiRicerca",
+    data(){
+        return{
+            genereUtente:"",
+        };
+    },
+    methods:{
+        avviaRicerca(){
+            this.$emit("inizioRicerca", this.genereUtente);
+        },
+    }
+     
+    
+}
+</script>
